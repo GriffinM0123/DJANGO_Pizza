be126bb47871ac03367410a6062cef8add6b65ef
Your Name <Griffin_McGuckin1@baylor.edu>
2022-05-06 04:46:03 -0500

    Final Project

---
+++ DjangoShell.py
@@ -0,0 +1,13 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Pizzeria.settings")
+
+import django
+django.setup()
+
+from pizzas.models import Pizza
+
+pizzas = Pizza.objects.all()
+
+for p in pizzas:
+    print(p.id, ' ', p)
+
